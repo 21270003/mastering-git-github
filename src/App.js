714a2b7a3af1e56bc7138dd0a31e6f479e310902
21270003 <rudddms1533@naver.com>
2024-10-15 11:48:04 +0900

    commit for action

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div align='center'>
+      <h1> 운영팀 웹 어드민 시스템</h1>
+      <h2> Welcome to 운영팀 웹 어드민 시스템 </h2>
+
+      <h2> 연락처: 051) 1234-3456 </h2>
+    </div>
+  );
+}
+
+export default App;
